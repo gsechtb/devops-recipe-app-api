b479cbe8dc82fbe237458f2f795c088aaef9ae01
issamafifi <issamafifi99@gmail.com>
2024-09-22 02:34:40 +0300

    Terraform configuration files and variables, fixing some files typos

--- infra/setup/main.tf
+++ infra/setup/main.tf
@@ -1,21 +1,20 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "5.23.0"
+      source    = "hashicorp/aws"
+      version   = "5.23.0"
     }
   }
 
   backend "s3" {
-    bucket = "issam-devops-recipe-app-tf-state"
-    key = "tf-state-setup"
-    region = "us-east-1"
-    encrypt = true
-    dynamodb_table = "issam-recipe-app-api-tf-lock"
+    bucket          = "issam-devops-recipe-app-tf-state"
+    key             = "tf-state-setup"
+    region          = "us-east-1"
+    encrypt         = true
+    dynamodb_table  = "issam-recipe-app-api-tf-lock"
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 
@@ -23,8 +22,8 @@ provider "aws" {
     tags = {
       Environment = terraform.workspace
       Project = var.project
-      contact = var.contact
-      ManagedBy = "Terraform/setup"
+      Contact = var.contact
+      ManageBy = "Terraform/setup"
     }
   }
 }
